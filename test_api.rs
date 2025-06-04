bb0936ff9a2a3e7845d9a9257469f396b9c2ac2c
James Bonanno <james@atlantixeng.com>
2025-06-04 08:27:07 -0400

    chore: clean up unnecessary files from dev process

--- test_api.rs
+++
@@ -1,7 +0,0 @@
-// Test file to discover gerber_viewer API
-use gerber_viewer::gerber_parser;
-
-fn main() {
-    // This will fail to compile but will show us what's actually available
-    let _ = gerber_parser::
-}
